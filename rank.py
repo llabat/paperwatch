7c4d42975fb5a33a8dd776d8679077af4ac4c9ab
Léo Labat <leo.labat@ens.fr>
2025-04-23 12:34:18 +0200

    bibtexpython parsing + progress bars added

--- rank.py
+++ rank.py
@@ -2,11 +2,11 @@ import re
 import sys
 import requests
 import numpy as np
+import bibtexparser
+from tqdm import tqdm
 from datetime import date
 from bs4 import BeautifulSoup
 from sentence_transformers import SentenceTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 def format_paper_info(paper_dict):
     title_str = "\\section{" + paper_dict["title"] + "} \\\\\n"
@@ -19,13 +19,14 @@ def generate_latex_file(papers, theme, date):
     entete = "\\documentclass{article}\n"
     packages = ["\\usepackage[english]{babel}", "\\usepackage[letterpaper,top=2cm,bottom=2cm,left=3cm,right=3cm,marginparwidth=1.75cm]{geometry}",
                 "\\usepackage{hyperref}", "\\usepackage{amsmath}", "\\usepackage{graphicx}", "\\usepackage[colorlinks=true, allcolors=blue]{hyperref}"] 
-    title_str = "\\title{Paper Watch (arXiv)\\\\ \\Large " + theme + "}\n\\author{Léo Labat}\n\\date{" + str(date) + "}\n\n"
+    title_str = "\\title{Paper Watch (arXiv)\\\\ \\Large " + theme + "}\n\\author{Leo Labat}\n\\date{" + str(date) + "}\n\n"
     beginner = "\\begin{document}\n\\maketitle\n"
     ender = "\\end{document}"
 
     return entete + "\n".join(packages) + title_str + beginner + "\n".join(format_paper_info(paper) for paper in papers) + ender
 
 def fetch_arxiv_cscl_new():
+    print("Scraping new arXiv papers...")
     url = 'https://arxiv.org/list/cs.CL/new'
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -45,6 +46,19 @@ def fetch_arxiv_cscl_new():
         })
     return papers
 
+def fetch_arxiv_abstract(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    
+    if "aclanthology" in url:
+        css_selector = ".acl-abstract span"
+    elif "arxiv" in url:
+        css_selector = ".abstract"
+    else:
+        raise ValueError(f"Unsupported website referenced in {url}")
+    
+    abstract_element = soup.select_one(css_selector)
+    return abstract_element.text.strip().replace("Abstract:", "") if abstract_element else None
 
 if __name__ == "__main__":
 
@@ -53,13 +67,14 @@ if __name__ == "__main__":
     seed_name = sys.argv[2]
 
     # Read bib file
-    with open(path2bib, "r") as f:
-        lines = f.readlines()
+    with open("mcq-seed.bib", 'r') as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
 
-    # Parse out the abstracts
-    abstract_regex =  r"(?<=abstractNote=\\{).+\.(?=\\\})"
-    seed_abstracts = [re.search(abstract_regex, lines[i]).group(0) if re.search(abstract_regex, lines[i]) else None 
-                    for i in range(len(lines))]
+    # Get urls from bib file
+    urls = [ref.get("url") for ref in bib_database.entries]
+
+    # Scrape abstracts
+    seed_abstracts = [fetch_arxiv_abstract(url) for url in tqdm(urls, desc="Scraping abstract seeds...")]
     
     # Collect all new arxiv papers
     new_papers = fetch_arxiv_cscl_new()
@@ -85,9 +100,4 @@ if __name__ == "__main__":
     with open(f"paper_watch_{seed_name.replace(' ', '-')}_{date.today()}.txt", "w") as f:
         reordered = [new_papers[i] for i in ranking]
         f.write(generate_latex_file(reordered, seed_name, date.today()))
-#        f.write(f"PAPER WATCH - arXiv - {date.today()}\nTheme : {seed_name}\n\n")
-#        for idx in ranking:
-#            for v in new_papers[idx].values():
-#                f.write(str(v) + "\n")
-#            f.write("\n")
 
